7fb6fbcf5492443442c7022e716081b162c5bd8b
Dmitry Ovchinnikov <mail@dimka.online>
2020-12-02 22:52:51 +0200

    Refactoring

--- test_02.py
+++ test_02.py
@@ -11,19 +11,20 @@ class TestDay02:
         cls.input_data = read_file_into_list('./inputs/02.txt')
         cls.regexpr = re.compile(r'^(\d+-\d+)\s([a-z]):\s(\w+$)')
 
-    def is_line_valid(self, input):
-        parsed_data = self.regexpr.match(input)
-        rule_range, rule_letter, password = parsed_data.groups()
-        rule_range = rule_range.split('-')
+    def parse_input_line(self, input_line):
+        parsed_data = self.regexpr.match(input_line)
+        rule, letter, password = parsed_data.groups()
+        rule = rule.split('-')
+        return (rule, letter, password)
 
-        return self.is_password_valid(rule_range, rule_letter, password)
+    def is_line_valid_v1(self, input_line):
+        rule, rule_letter, password = self.parse_input_line(input_line)
 
-    def is_password_valid(self, rule_range, rule_letter, password):
         letters_count = len([letter for letter in password if letter == rule_letter])
-        return letters_count >= int(rule_range[0]) and letters_count <= int(rule_range[1])
+        return letters_count >= int(rule[0]) and letters_count <= int(rule[1])
 
     @validate_result(546)
     def test_challenge_01(self):
-        valid_lines = [line for line in self.input_data if self.is_line_valid(line)]
+        valid_lines = [line for line in self.input_data if self.is_line_valid_v1(line)]
 
         return len(valid_lines)
